c978736c526ccb22961b439733bc16a77d777f77
Dilshad <hmydilshadkt@gmail.com>
2025-01-25 17:40:06 +0530

    feat : change landpage

--- src/components/landpage/index.jsx
+++ src/components/landpage/index.jsx
@@ -21,7 +21,7 @@ const Landpage = () => {
         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
           <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
             <img
-              src="https://images.unsplash.com/photo-1542340916-951bb72c8f74?auto=format&q=75&fit=crop&w=550&h=550"
+              src="/images/landpage.jpg"
               loading="lazy"
               alt="Photo by Kaung Htet"
               className="h-full w-full object-cover object-center"
@@ -30,7 +30,7 @@ const Landpage = () => {
 
           <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
             <img
-              src="https://images.unsplash.com/photo-1586295166487-b265f7e83a7f?auto=format&q=75&fit=crop&w=550&h=550"
+              src="/images/landpage2.jpg"
               loading="lazy"
               alt="Photo by Manny Moreno"
               className="h-full w-full object-cover object-center"
